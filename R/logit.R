82a300c17ba8ad7d9d854e1e1353230bafcc2182
AJK <AustinKnuppe@dhcp-164-107-236-45.osuwireless.ohio-state.edu>
2015-03-24 11:11:52 -0400

    added an example of running a basic logit with network data

--- R/logit.R
+++ R/logit.R
@@ -1,3 +1,22 @@
-source("R/clean-data.R")
+# source("R/clean-data.R")
+
+## Example of estimating a basic logit with network data
+
+library("statnet")
+
+A <- matrix(sample(0:1, 100, replace=TRUE), ncol=10, nrow=10)
+
+colnames(A) <- LETTERS[1:10]
+
+A
+
+N <- network(A, bipartite=TRUE)
+
+N
+
+plot(N)
+
+net_logit <- ergmMPLE(N ~ edges + b1star(2:5), output="matrix")
+
 
 
