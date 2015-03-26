8ec9208f4c34795e93d5132f07382a39a2954643
Jason W. Morgan <jwm@skepsi.net>
2015-03-26 10:32:13 -0400

    Code cleanup and questions added as comments

--- R/prelim-ergm.R
+++ R/prelim-ergm.R
@@ -37,42 +37,34 @@ summary(mod2)
 mod3 <- ergm(JSFnets[[1]] ~ edges + b2star(2), control=ctrl, verbose=TRUE)
 summary(mod3)
 
-# Model with b2 stars and b1 degree 
+
+## JWM: Where is N defined?
+# Model with b2 stars and b1 degree
 degreedist <- table(degree(N))
 
+## What is JSF? I can't pull dollarsobligated out of it:
+## > JSF$dollarsobligated
+## Error in JSF$dollarsobligated : $ operator is invalid for atomic vectors
 attr <- as.list(as.numeric(JSF$dollarsobligated))
 
 set.vertex.attribute(N, names(attr), attr)
 
+## JWM: What is an empty edgecov?
 mod4 <- ergm(N ~ edges + b1degree(2:4) + b2star(3) + edgecov(""))
 
 
-DECAY <- seq(1, 4, by=0.2)
-RESULT <- matrix(0, ncol=2, nrow=length(DECAY))
+## -----------------------------------------------------------------------------
+## JWM: Merge the all the JSF networks, look at the network
 
-for (i in 1:length(DECAY)) {
-    decay <- DECAY[i]
-    RESULT[i,] <- try(coef(ergm(JSFnets[[1]] ~ edges
-                                ## + gwb1degree(3.0, fixed=TRUE)
-                                + gwb2degree(decay, fixed=TRUE),
-                                verbose=TRUE, control=ctrl)))
+fix_adj <- function(adj)
+{
+    rownames(adj) <- adj[,1]
+    adj[,-1]
 }
 
-## > RESULT
-##            [,1]       [,2]
-##  [1,]  1.517632  -6.944632
-##  [2,]  1.578917  -4.785641
-##  [3,]  2.045375  -9.890707
-##  [4,]  2.466362  -8.483947
-##  [5,]  2.927742  -9.103538
-##  [6,]  3.434409 -10.359861
-##  [7,]  3.776188  -9.870859
-##  [8,]  4.276437 -10.577873
-##  [9,]  4.666221 -10.473028
-## [10,]  5.176068 -10.507367
-## [11,]  5.622305 -10.716358
-## [12,]  6.284993 -11.065329
-## [13,]  7.024273 -11.621657
-## [14,]  7.950342 -12.408078
-## [15,]  9.117513 -13.468616
-## [16,] 10.530569 -14.883984
+Adj <- lapply(JSFadj, fix_adj)
+FullNet <- network(Reduce(`+`, Adj), directed=FALSE, bipartite=TRUE)
+
+## JWM: Why do we have isolates? A CD or agency can't get into the data set if
+## it is an isolate, right?
+plot(FullNet)
