c43249d3b71502f13ec8a9cd3271e92bbf4984dd
asrosenberg <asrosenberg90@Macintosh.local>
2015-04-12 09:17:03 -0400

    Started doing some TERGMs

--- R/final-tergm-analysis.R
+++ R/final-tergm-analysis.R
@@ -1,22 +1,33 @@
 library(xergm)
+library(texreg)
 
 load(file="final-time-slices.RData")
 
+# Add Committee, Contracts and Contributions as vertex attributes
 
-JSFnets_0 <- list(JSFnets[[2]], JSFnets[[3]], JSFnets[[4]], JSFnets[[5]], JSFnets[[6]], JSFnets[[7]])
-JSFnets_1 <- list(JSFnets[[1]], JSFnets[[2]], JSFnets[[3]], JSFnets[[4]], JSFnets[[5]], JSFnets[[6]])
+for (i in 1:length(final_JSFnets)){
+     final_JSFnets[[i]] <- set.vertex.attribute(final_JSFnets[[i]], "Committee", final_JSF$Committee) # add as vertex attribute     
+     final_JSFnets[[i]] <- set.vertex.attribute(final_JSFnets[[i]], "Contracts", final_JSF$ln_contracts) # add as vertex attribute     
+     final_JSFnets[[i]] <- set.vertex.attribute(final_JSFnets[[i]], "Contributions", final_JSF$ln_contrib) # add as vertex attribute     
+}
 
 
-fit <- btergm(JSFnets ~ edges, R = 100)
-summary(fit)
+final_JSFnets_0 <- list(final_JSFnets[[2]], final_JSFnets[[3]], final_JSFnets[[4]], 
+                        final_JSFnets[[5]], final_JSFnets[[6]], final_JSFnets[[7]])
+final_JSFnets_1 <- list(final_JSFnets[[1]], final_JSFnets[[2]], final_JSFnets[[3]], 
+                        final_JSFnets[[4]], final_JSFnets[[5]], final_JSFnets[[6]])
 
-fit <- btergm(JSFnets_0 ~ edges + edgecov(JSFnets_1), 
+
+#fit <- btergm(final_JSFnets ~ edges, R = 100)
+#summary(fit)
+
+fit <- btergm(final_JSFnets_0 ~ edges + edgecov(final_JSFnets_1), 
               R = 100)
 summary(fit)
 
-fit <- btergm(JSFnets_0 ~ edges +
+fit <- btergm(final_JSFnets_0 ~ edges +
                    b2star(2:3) + 
-                   edgecov(JSFnets_1), 
+                   edgecov(final_JSFnets_1), 
               R = 1000)
 summary(fit)
 
@@ -42,4 +53,29 @@ fit <- btergm(JSFnets_0 ~ edges +
                    edgecov(JSFnets_1) +
                    edgecov(JSFnets_1, attrname = "dollars"), 
               R = 1000)
+summary(fit) 
+
+# TERGM with each new vertex attribute
+fit <- btergm(final_JSFnets_0 ~ edges +
+                   b2star(2:3) + 
+                   gwb1degree(.5, fixed = TRUE) +
+                   edgecov(final_JSFnets_1) + nodecov("Committee"), 
+              R = 1000)
+summary(fit) 
+
+fit <- btergm(final_JSFnets_0 ~ edges +
+                   b2star(2:3) + 
+                   edgecov(final_JSFnets_1) + nodecov("Contributions") + 
+                   nodecov("Contracts") + nodecov("Committee"), 
+              R = 10000)
 summary(fit)
+
+fit <- btergm(final_JSFnets_0 ~ edges +
+                   b2star(2:3) + 
+                   edgecov(final_JSFnets_1) + nodecov("Contributions") + 
+                   nodecov("Contracts"), 
+              R = 1000)
+summary(fit, level = 0.95)
+gof(fit)
+
+screenreg(fit)
