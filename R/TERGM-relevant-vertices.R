31e5a89e9b5a7f5836236c91e0a58d3e9777fa13
asrosenberg <asrosenberg90@dhcp-140-254-90-70.osuwireless.ohio-state.edu>
2015-04-23 12:08:27 -0400

    Added relevant CDs and agencies to network as 0s so we aren't selecting on DV, then ran tergms. Saved tergm results as tergmfits.RData

--- R/TERGM-relevant-vertices.R
+++ R/TERGM-relevant-vertices.R
@@ -1,9 +1,42 @@
+#______________________________________________________________________________
+#______________________________________________________________________________
+# Let's add a vector of 0s for each of our time slices
+#______________________________________________________________________________
+#______________________________________________________________________________
+library(network)
+
+setwd("~/Dropbox/DoD/data")
+load("relevant_cd.RData")
+load("relevant_agencies.RData")
+load("final-time-slices.RData")
+
+# We need to see if the network package can auto add nodes to our bipartite net
+add.vertices(final_JSFnets[[1]], nv = 16, last.mode = FALSE)
+add.vertices(final_JSFnets[[2]], nv = 16, last.mode = FALSE)
+add.vertices(final_JSFnets[[3]], nv = 16, last.mode = FALSE)
+add.vertices(final_JSFnets[[4]], nv = 16, last.mode = FALSE)
+add.vertices(final_JSFnets[[5]], nv = 16, last.mode = FALSE)
+add.vertices(final_JSFnets[[6]], nv = 16, last.mode = FALSE)
+add.vertices(final_JSFnets[[7]], nv = 16, last.mode = FALSE)
+
+add.vertices(final_JSFnets[[1]], nv = 335, last.mode = TRUE)
+add.vertices(final_JSFnets[[2]], nv = 335, last.mode = TRUE)
+add.vertices(final_JSFnets[[3]], nv = 335, last.mode = TRUE)
+add.vertices(final_JSFnets[[4]], nv = 335, last.mode = TRUE)
+add.vertices(final_JSFnets[[5]], nv = 335, last.mode = TRUE)
+add.vertices(final_JSFnets[[6]], nv = 335, last.mode = TRUE)
+add.vertices(final_JSFnets[[7]], nv = 335, last.mode = TRUE)
+
+#______________________________________________________________________________
+#______________________________________________________________________________
+# Ok let's copy the analysis section below and see if TERGMs work
+#______________________________________________________________________________
+#______________________________________________________________________________
+
 library(xergm)
 library(texreg)
 library(network)
 
-setwd("~/Dropbox/DoD/Data")
-load(file = "final-time-slices.RData") 
 load(file = "final_JSF_data.RData")
 
 # Add Committee, Contracts and Contributions as vertex attributes
@@ -67,96 +100,14 @@ final_JSFnets[[7]] <- set.vertex.attribute(final_JSFnets[[7]], "contrib",
 final_JSFnets[[7]] <- set.vertex.attribute(final_JSFnets[[7]], "Committee", 
                                            final_JSF_112$Committee)
 
-
-
-#____________________________________________________________________________________
-#____________________________________________________________________________________
-# Ok we need to make sure only mode 2 nodes have attributes
-#____________________________________________________________________________________
-#____________________________________________________________________________________
-slice_one_names <- as.vector(final_JSFnets[[1]] %v% "vertex.names")
-slice_two_names <- as.vector(final_JSFnets[[2]] %v% "vertex.names")
-slice_three_names <- as.vector(final_JSFnets[[3]] %v% "vertex.names")
-slice_four_names <- as.vector(final_JSFnets[[4]] %v% "vertex.names")
-
-for(i in 1:length(final_JSFnets)){
-     tmp <- (final_JSFnets[[i]] %v% "contrib")
-     tmp[88:94] <- NA
-     final_JSFnets[[i]] %v% "contrib" <- tmp
-}
-
-for(i in 1:length(final_JSFnets)){
-     tmp <- (final_JSFnets[[i]] %v% "contracts")
-     tmp[88:94] <- NA
-     final_JSFnets[[i]] %v% "contracts" <- tmp
-}
-
-for(i in 1:length(final_JSFnets)){
-     tmp <- (final_JSFnets[[i]] %v% "Committee")
-     tmp[88:94] <- NA
-     final_JSFnets[[i]] %v% "Committee" <- tmp
-}
-
 final_JSFnets_0 <- list(final_JSFnets[[2]], final_JSFnets[[3]], final_JSFnets[[4]], 
                         final_JSFnets[[5]], final_JSFnets[[6]], final_JSFnets[[7]])
 final_JSFnets_1 <- list(final_JSFnets[[1]], final_JSFnets[[2]], final_JSFnets[[3]], 
                         final_JSFnets[[4]], final_JSFnets[[5]], final_JSFnets[[6]])
 
 
-save(final_JSFnets, final_JSFnets_0, final_JSFnets_1, file = "analysis.RData")
-
-
-#fit <- btergm(final_JSFnets ~ edges, R = 100)
-#summary(fit)
-
-fit <- btergm(final_JSFnets_0 ~ edges + edgecov(final_JSFnets_1), 
-              R = 100)
-summary(fit)
-
-fit1 <- btergm(final_JSFnets_0 ~ edges +
-                   b2star(2:3) + 
-                   edgecov(final_JSFnets_1), 
-              R = 1000)
-summary(fit1)
-
-model <- btergm(final_JSFnets_0 ~ edges +
-                   gwb1degree(.5, fixed = TRUE) +
-                   edgecov(final_JSFnets_1), 
-              R = 1000)
-summary(model)
-
-
-fit3 <- btergm(final_JSFnets_0 ~ edges +
-                   b2star(2:3) + 
-                   gwb1degree(.5, fixed = TRUE) +
-                   edgecov(final_JSFnets_1), 
-              R = 1000)
-summary(fit3)
-gof3 <- gof(fit3)
-
-
-# add dollars (or don't)
-fit <- btergm(JSFnets_0 ~ edges +
-                   b2star(2:3) + 
-                   gwb1degree(.5, fixed = TRUE) +
-                   edgecov(JSFnets_1) +
-                   edgecov(JSFnets_1, attrname = "dollars"), 
-              R = 1000)
-summary(fit) 
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-# Analysis from here
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-# TERGM with each new vertex attribute
-library(xergm)
-library(texreg)
-library(network)
-setwd("~/Dropbox/DoD/data")
-load("analysis.RData")
-
 fit1 <- btergm(final_JSFnets_0 ~ edges + edgecov(final_JSFnets_1)
-                  + b2star(2:3), R = 1000)
+               + b2star(2:3), R = 1000)
 
 summary(fit1) 
 screenreg(fit1)
@@ -168,25 +119,13 @@ fit2 <- btergm(final_JSFnets_0 ~ edges +
 
 summary(fit2)
 screenreg(fit2)
-gof(fit2)
 
 fit3 <- btergm(final_JSFnets_0 ~ edges +
                     b2star(2:3) + 
                     nodefactor("Committee")
                + edgecov(final_JSFnets_1, attrname = "dollars"), 
                R = 1000)
-
-summary(fit3, level = 0.90)
-screenreg(fit3)
-<<<<<<< HEAD
-gof3 <- gof(fit3)
-=======
-gof_3 <- gof(fit3, nsim = 25) # Want BIG p-values here
-
-pdf("gof_3.pdf")
-plot(gof_3)
-dev.off()
->>>>>>> 72a675111ea94a0d6ea1b847550d3e79c89a8950
+summary(fit3)
 
 fit4 <- btergm(final_JSFnets_0 ~ edges +
                     b2star(2:3) + 
@@ -195,8 +134,6 @@ fit4 <- btergm(final_JSFnets_0 ~ edges +
                + nodecov("contrib"), 
                R = 1000)
 summary(fit4)
-screenreg(fit4)
-plotreg(list(fit3, fit4))
 
 fit5 <- btergm(final_JSFnets_0 ~ edges +
                     b2star(2:3) + 
@@ -205,24 +142,14 @@ fit5 <- btergm(final_JSFnets_0 ~ edges +
                + nodecov("contracts"), 
                R = 1000)
 summary(fit5)
-screenreg(fit4)
 
+# oh shit these tergm's work so let's save them
+save(fit1, fit2, fit3, fit4, fit5, file ="tergmfits.RData")
 
-fit <- btergm(final_JSFnets_0 ~ edges +
-                   b2star(2:3) + 
-                   nodecov("contrib") + 
-                   nodecov("contracts") + nodecov("Committee"), 
-              R = 1000)
-summary(fit)
-
-final_JSFnets_0[[1]] %v% "Contributions"
-final_JSFnets_0[[1]] %v% "Contributions" <- factor(final_JSFnets_0[[1]] %v% "Contributions")
-fit <- btergm(final_JSFnets_0 ~ edges + b2star(2:3) + 
-                   edgecov(final_JSFnets_1) + 
-                   nodefactor(as.factor(as.character(("Contributions"))) + 
-               nodefactor(as.factor(as.character("Contracts"))) + 
-                    nodefactor("Committee"), R = 1000))
-summary(fit, level = 0.95)
-gof(fit)
-
-screenreg(fit)
+fit6 <- btergm(final_JSFnets_0 ~ edges +
+                    b2star(2:3) + 
+                    nodefactor("Committee")
+               + edgecov(final_JSFnets_1, attrname = "dollars")
+               + nodecov("contracts") + nodecov("contrib"), 
+               R = 1000)
+summary(fit6)
