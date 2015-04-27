1ed733991234b7977fb0c7c70237df8c53d26cb0
AJK <AustinKnuppe@dhcp-164-107-233-86.osuwireless.ohio-state.edu>
2015-04-27 13:04:50 -0400

    saving btergm models

--- R/TERGM-relevant-vertices.R
+++ R/TERGM-relevant-vertices.R
@@ -116,28 +116,30 @@ plot(final_JSFnets[[6]])
 plot(final_JSFnets[[7]])
 
 
-## a naive model specification
-fit0 <- btergm(final_JSFnets_1 ~ edges + b2star(2:3), R = 1000)
-gof(fit0)
 
-adj1 <- as.matrix(final_JSFnets[[1]])
-adj2 <- as.matrix(final_JSFnets[[2]])
-adj3 <- as.matrix(final_JSFnets[[3]])
-adj4 <- as.matrix(final_JSFnets[[4]])
-adj5 <- as.matrix(final_JSFnets[[5]])
-adj6 <- as.matrix(final_JSFnets[[6]])
-adj7 <- as.matrix(final_JSFnets[[7]])
-adj <- list(adj1, adj2, adj3, adj4, adj5, adj6, adj7)
+
+#adj1 <- as.matrix(final_JSFnets[[1]])
+#adj2 <- as.matrix(final_JSFnets[[2]])
+#adj3 <- as.matrix(final_JSFnets[[3]])
+#adj4 <- as.matrix(final_JSFnets[[4]])
+#adj5 <- as.matrix(final_JSFnets[[5]])
+#adj6 <- as.matrix(final_JSFnets[[6]])
+#adj7 <- as.matrix(final_JSFnets[[7]])
+#adj <- list(adj1, adj2, adj3, adj4, adj5, adj6, adj7)
 
 for (i in 1:length(adj)){
      adj[[i]] <- network(adj[[i]])
 }
-fit0 <- btergm(adj ~ edges + b2star(2:3), R = 1000)
+
+## a naive model specification
+fit0 <- btergm(final_JSFnets ~ edges + b2star(2:3), R = 1000)
+screenreg(fit0)
 gof(fit0)
 
 ## A basic model with only endogenous network terms:
-fit1 <- btergm(final_JSFnets_0 ~ edges + edgecov(final_JSFnets_1)
-                + b2star(2:3), R = 1000)
+fit1 <- btergm(final_JSFnets ~ edges + 
+                     b2star(2:3) +
+                     edgecov(final_JSFnets, attrname = "dollars"), R = 1000)
 
 summary(fit1) 
 screenreg(fit1)
@@ -145,8 +147,8 @@ gof(fit1)
 
 
 ## A model where we had the dummy for committee:
-fit2 <- btergm(final_JSFnets_0 ~ edges +
-                    edgecov(final_JSFnets_1) +
+fit2 <- btergm(final_JSFnets ~ edges +
+                    edgecov(final_JSFnets, attrname = "dollars") +
                     b2star(2:3) + 
                     nodefactor("Committee"), R = 1000)
 
@@ -154,187 +156,53 @@ summary(fit2)
 screenreg(fit2)
 gof(fit2)
 
-# Let's add contract dollar value as an edge-level covariate
-fit3 <- btergm(final_JSFnets_0 ~ edges +
-                    b2star(2:3) + 
-                    nodefactor("Committee") +
-                    edgecov(final_JSFnets_1, attrname = "dollars"), R = 1000)
-summary(fit3)
-screenreg(fit3)
-gof(fit3)
 
 # Let's add campaign contributions as an exogeneous covariate 
-fit4 <- btergm(final_JSFnets_0 ~ edges +
+fit3 <- btergm(final_JSFnets ~ edges +
                     b2star(2:3) + 
-                    edgecov(final_JSFnets_1, attrname = "dollars") +
+                    edgecov(final_JSFnets, attrname = "dollars") +
                     nodefactor("Committee") +
                     nodecov("contrib"), R = 1000)
 
-summary(fit4)
-screenreg(list(fit4, fit7))
-gof(fit4)
+summary(fit3)
+screenreg(list(fit3)
+gof(fit3)
 
 # Let's add comparative advantage as an exogeneous covariate 
-fit5 <- btergm(final_JSFnets_0 ~ edges +
+fit4 <- btergm(final_JSFnets ~ edges +
                     b2star(2:3) + 
                     nodefactor("Committee") +
-                    edgecov(final_JSFnets_1, attrname = "dollars") +
+                    edgecov(final_JSFnets, attrname = "dollars") +
                     nodecov("contracts"), R = 1000)
-summary(fit5)
-screenreg(fit5)
-gof(fit5)
+summary(fit4)
+screenreg(fit4)
+gof(fit4)
 
-## If we change the outcome network to final_JSFnets_1 we can run a model with
-## both contracts and contributions
-
-fit6 <- btergm(final_JSFnets_1 ~ edges +
-                    b2star(2:3) + 
-                    edgecov(final_JSFnets_1, attrname = "dollars") +
-                    nodefactor("Committee") +
-                    nodecov("contracts") + 
-                    nodecov("contrib"), R = 1000)
-summary(fit6)
-screenreg(fit6)
-gof(fit6)
-
-## without comparative advantage:
-fit6b <- btergm(final_JSFnets_1 ~ edges +
-                    b2star(2:3) + 
-                    edgecov(final_JSFnets_1, attrname = "dollars") +
-                    nodefactor("Committee") + 
-                    nodecov("contrib"), R = 1000)
-summary(fit6b)
-screenreg(fit6b)
-gof(fit6b)
-
-
-
-## A model specification with exogenous covars no lagged IV...
-fit7 <- btergm(final_JSFnets ~ edges +
+## A model specification with the works... 
+fit5 <- btergm(final_JSFnets ~ edges +
                                b2star(2:3) + 
                                edgecov(final_JSFnets, attrname = "dollars") +
                                nodefactor("Committee") +
                                nodecov("contracts") + 
                                nodecov("contrib"), R = 1000)
 
-summary(fit7)
+summary(fit5)
+screenreg(fit5)
 
-screenreg(fit7)
+gof5 <- gof(fit5)
+plot(gof5, roc = TRUE, pr = TRUE)
 
-gof7 <- gof(fit7, target = final_JSFnets,
-        formula = getformula(fit7), nsim = 100, MCMC.interval = 1000,
-        MCMC.burnin = 10000, classicgof = TRUE,
-        rocprgof = TRUE, checkdegeneracy = TRUE,
-        dsp = TRUE, esp = TRUE, geodist = TRUE, degree = TRUE,
-        idegree = FALSE, odegree = FALSE, kstar = TRUE, istar = FALSE,
-        ostar = FALSE, pr.impute = "poly4", verbose = TRUE)
+gof5
 
-plot(gof7, roc = TRUE, pr = TRUE)
-
-gof7
-
-plot(gof7, boxplot = FALSE, roc = TRUE, pr = FALSE, 
+plot(gof5, boxplot = FALSE, roc = TRUE, pr = FALSE, 
      roc.random = TRUE, ylab = "TPR/PPV", 
      xlab = "FPR/TPR", roc.main = "ROC and PR curves")
 
-plot(gof7, boxplot = FALSE, roc = FALSE, pr = TRUE, 
+plot(gof5, boxplot = FALSE, roc = FALSE, pr = TRUE, 
      pr.random = TRUE, rocpr.add = TRUE)
-
-## Let's try a model specification with ln of contributions and comp. ad.:
-
-final_JSFnets[[1]] <- set.vertex.attribute(final_JSFnets[[1]], "ln_dollars", 
-                                           final_JSF_109$contracts)
-final_JSFnets[[1]] <- set.vertex.attribute(final_JSFnets[[1]], "ln_ammount", 
-                                           final_JSF_109$contrib)
-final_JSFnets[[1]] <- set.vertex.attribute(final_JSFnets[[1]], "Committee", 
-                                           final_JSF_109$Committee)
-# slice 2 is 110th
-final_JSFnets[[2]] <- set.vertex.attribute(final_JSFnets[[2]], "ln_dollars", 
-                                           final_JSF_110$contracts)
-final_JSFnets[[2]] <- set.vertex.attribute(final_JSFnets[[2]], "ln_ammount", 
-                                           final_JSF_110$contrib)
-final_JSFnets[[2]] <- set.vertex.attribute(final_JSFnets[[2]], "Committee", 
-                                           final_JSF_110$Committee)
-# slice 3 is 110th
-final_JSFnets[[3]] <- set.vertex.attribute(final_JSFnets[[3]], "ln_dollars", 
-                                           final_JSF_110$contracts)
-final_JSFnets[[3]] <- set.vertex.attribute(final_JSFnets[[3]], "ln_ammount", 
-                                           final_JSF_110$contrib)
-final_JSFnets[[3]] <- set.vertex.attribute(final_JSFnets[[3]], "Committee", 
-                                           final_JSF_110$Committee)
-# slice 4 is 111th
-final_JSFnets[[4]] <- set.vertex.attribute(final_JSFnets[[4]], "ln_dollars", 
-                                           final_JSF_111$contracts)
-final_JSFnets[[4]] <- set.vertex.attribute(final_JSFnets[[4]], "ln_ammount", 
-                                           final_JSF_111$contrib)
-final_JSFnets[[4]] <- set.vertex.attribute(final_JSFnets[[4]], "Committee", 
-                                           final_JSF_111$Committee)
-# slice 5 is 111th
-final_JSFnets[[5]] <- set.vertex.attribute(final_JSFnets[[5]], "ln_dollars", 
-                                           final_JSF_111$contracts)
-final_JSFnets[[5]] <- set.vertex.attribute(final_JSFnets[[5]], "ln_ammount", 
-                                           final_JSF_111$contrib)
-final_JSFnets[[5]] <- set.vertex.attribute(final_JSFnets[[5]], "Committee", 
-                                           final_JSF_111$Committee)
-# slice 6 is 112th
-final_JSFnets[[6]] <- set.vertex.attribute(final_JSFnets[[6]], "ln_dollars", 
-                                           final_JSF_112$contracts)
-final_JSFnets[[6]] <- set.vertex.attribute(final_JSFnets[[6]], "ln_ammount", 
-                                           final_JSF_112$contrib)
-final_JSFnets[[6]] <- set.vertex.attribute(final_JSFnets[[6]], "Committee", 
-                                           final_JSF_112$Committee)
-# slice 7 is 112th
-final_JSFnets[[7]] <- set.vertex.attribute(final_JSFnets[[7]], "ln_dollars", 
-                                           final_JSF_112$contracts)
-final_JSFnets[[7]] <- set.vertex.attribute(final_JSFnets[[7]], "ln_ammount", 
-                                           final_JSF_112$contrib)
-final_JSFnets[[7]] <- set.vertex.attribute(final_JSFnets[[7]], "Committee", 
-                                           final_JSF_112$Committee)
-
-## fit 8
-fit8 <- btergm(final_JSFnets_0 ~ edges +
-                    b2star(2:3) + 
-                    edgecov(final_JSFnets_, attrname = "dollars") +
-                    nodefactor("Committee") +
-                    nodecov("ln_dollars") + 
-                    nodecov("ln_ammount"), R = 1000)
-
-summary(fit8)
-
-screenreg(fit8)
-
-gof8 <- gof(fit7, target = final_JSFnets,
-            formula = getformula(fit7), nsim = 100, MCMC.interval = 1000,
-            MCMC.burnin = 10000, classicgof = TRUE,
-            rocprgof = TRUE, checkdegeneracy = TRUE,
-            dsp = TRUE, esp = TRUE, geodist = TRUE, degree = TRUE,
-            idegree = FALSE, odegree = FALSE, kstar = TRUE, istar = FALSE,
-            ostar = FALSE, pr.impute = "poly4", verbose = TRUE)
-
-plot(gof8, roc = TRUE, pr = TRUE)
-
-gof8
-
-plot(gof8, boxplot = FALSE, roc = TRUE, pr = FALSE, 
-     roc.random = TRUE, ylab = "TPR/PPV", 
-     xlab = "FPR/TPR", roc.main = "ROC and PR curves")
-
-plot(gof8, boxplot = FALSE, roc = FALSE, pr = TRUE, 
-     pr.random = TRUE, rocpr.add = TRUE)
-
-
-
-
-
-
-
-
-
-
-
 
 
 # oh shit these tergm's work so let's save them
-save(fit1, fit2, fit3, fit4, fit5, fit6, fit7, gof7, file ="tergmfits.RData")
+save(fit0, fit1, fit2, fit3, fit4, fit5, file ="tergmfits.RData")
 
-load("tergmfits.RData")
+#load("tergmfits.RData")
